610bea375bfab143781b7ab9c0a1e63dca4e7f16
Wes Peacock <wes_peacock@sil.org>
2024-12-12 15:56:29 -0700

    Ucode pass SFM markers in a command line option
     - --help option prints a USAGE line
     - default lx,se,xe

--- Ucode.pl
+++ Ucode.pl
@@ -1,7 +1,9 @@
 #!/usr/bin/perl
-# perl Ucode.pl
+my $USAGE = "Usage: $0 [--sfms lx,se,xv] [--help]";
+# perl Ucode.pl --sfms lx,se,xv
 # convert the text of venacular fields to Unicode U+XXXXX format
-# The SFM fields to be converted are specified in $vernacularSFMs
+# The SFM fields to be converted are specified in a comma separated
+# string by the --sfms option; the default is lx,se,xv
 # 
 # Originally planned as a scheme for Unicode planes that cannot be imported by the FLEx SFM import.
 #  I.e., convert the fields to U+XXXXX format, import them inot FLEx and use tthe perl code execution in FLEx to decode
@@ -12,7 +14,19 @@
 use 5.016;
 use strict;
 use open qw(:std :utf8);
-my $vernacularSFMs = qr{lx|xe|se};
+use Getopt::Long;
+GetOptions (
+	'sfms:s'   => \(my $sfms = "lx,se,xv"), # Standard format fields to correct
+	'help'    => \my $help,
+	) or die $USAGE;
+
+if ($help) {
+	say STDERR $USAGE;
+	exit;
+	}
+
+$sfms =~  s/,/\|/g;
+my $vernacularSFMs = qr{$sfms};
 while (my $line = <>) {
    if ($line =~ m/(\\($vernacularSFMs) )(.*?)(\R)/) {
 	   my $sfm=$2;
